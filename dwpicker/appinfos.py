33002b192e506c58cda155bacacbd05cb2846ee0
Lionel Brouyere <lionel.brouyere@gmail.com>
2022-03-24 09:43:13 +0100

    Release 0.4.1

--- dwpicker/appinfos.py
+++ dwpicker/appinfos.py
@@ -1,4 +1,4 @@
-VERSION = 0, 4, 0  # Version, Feature, Hotfix
-RELEASE_DATE = 'March 23th 2022'
+VERSION = 0, 4, 1  # Version, Feature, Hotfix
+RELEASE_DATE = 'March 24th 2022'
 DW_WEBSITE = 'https://fr.dreamwall.be/'
 DW_GITHUB = 'https://github.com/DreamWall-Animation'
