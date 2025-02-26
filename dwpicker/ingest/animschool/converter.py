5b3774e81434a988205873bdda4815c8e23c69cc
Lionel Brouyère <brouyere@dreamwall.be>
2025-02-26 09:55:45 +0100

    fix anim school importer

--- dwpicker/ingest/animschool/converter.py
+++ dwpicker/ingest/animschool/converter.py
@@ -2,8 +2,156 @@ import json
 import os
 from dwpicker.pyside import QtGui
 
-from dwpicker.templates import PICKER, BUTTON, BACKGROUND
+from dwpicker.compatibility import ensure_retro_compatibility
 from dwpicker.ingest.animschool.parser import parse_animschool_picker, save_png
+
+
+PICKER = {
+    'name': 'Untitled',
+    'version': (0, 15, 3),
+    'panels.as_sub_tab': False,
+    'panels.orientation': 'vertical',
+    'panels.zoom_locked': [False],
+    'panels.colors': [None],
+    'panels.names': ['Panel 1'],
+    'menu_commands': [],
+    'hidden_layers': [],
+    'panels': [[1.0, [1.0]]]
+}
+
+BUTTON = {
+    'background': False,
+    'visibility_layer': None,
+    'shape.ignored_by_focus': False,
+    'panel': 0,
+    'shape': 'square',  # or round or rounded_rect or custom
+    'shape.space': 'world',  # or screen
+    'shape.anchor': 'top_left',  # or bottom_left, top_right, bottom_right
+    'shape.path' : [],
+    'shape.left': 0.0,
+    'shape.top': 0.0,
+    'shape.width': 120.0,
+    'shape.height': 25.0,
+    'shape.cornersx': 4,
+    'shape.cornersy': 4,
+    'border': True,
+    'borderwidth.normal': 1.0,
+    'borderwidth.hovered': 1.25,
+    'borderwidth.clicked': 2,
+    'bordercolor.normal': '#000000',
+    'bordercolor.hovered': '#393939',
+    'bordercolor.clicked': '#FFFFFF',
+    'bordercolor.transparency': 0,
+    'bgcolor.normal': '#888888',
+    'bgcolor.hovered': '#AAAAAA',
+    'bgcolor.clicked': '#DDDDDD',
+    'bgcolor.transparency': 0,
+    'text.content': 'Button',
+    'text.size': 12,
+    'text.bold': False,
+    'text.italic': False,
+    'text.color': '#FFFFFF',
+    'text.valign': 'center',  # or 'top' or bottom
+    'text.halign': 'center',  # or 'left' or 'right'
+    'action.targets': [],
+    'action.commands': [],
+    'action.menu_commands': [],
+    'image.path': '',
+    'image.fit': True,
+    'image.ratio': True,
+    'image.height': 32,
+    'image.width': 32
+}
+
+
+TEXT = {
+    'background': False,
+    'visibility_layer': None,
+    'shape.ignored_by_focus': False,
+    'panel': 0,
+    'shape': 'square',  # or round or rounded_rect or custom
+    'shape.space': 'world',  # or screen
+    'shape.anchor': 'top_left',  # or bottom_left, top_right, bottom_right
+    'shape.path' : [],
+    'shape.left': 0.0,
+    'shape.top': 0.0,
+    'shape.width': 200.0,
+    'shape.height': 50.0,
+    'shape.cornersx': 4,
+    'shape.cornersy': 4,
+    'border': False,
+    'borderwidth.normal': 0,
+    'borderwidth.hovered': 0,
+    'borderwidth.clicked': 0,
+    'bordercolor.normal': '#000000',
+    'bordercolor.hovered': '#393939',
+    'bordercolor.clicked': '#FFFFFF',
+    'bordercolor.transparency': 0,
+    'bgcolor.normal': '#888888',
+    'bgcolor.hovered': '#AAAAAA',
+    'bgcolor.clicked': '#DDDDDD',
+    'bgcolor.transparency': 255,
+    'text.content': 'Text',
+    'text.size': 16,
+    'text.bold': True,
+    'text.italic': False,
+    'text.color': '#FFFFFF',
+    'text.valign': 'top',  # or 'top' or bottom
+    'text.halign': 'left',  # or 'left' or 'right'
+    'action.targets': [],
+    'action.commands': [],
+    'action.menu_commands': [],
+    'image.path': '',
+    'image.fit': False,
+    'image.ratio': True,
+    'image.height': 32,
+    'image.width': 32,
+}
+
+
+BACKGROUND = {
+    'background': True,
+    'visibility_layer': None,
+    'shape.ignored_by_focus': True,
+    'panel': 0,
+    'shape': 'square',  # or round or rounded_rect or custom
+    'shape.space': 'world',  # or screen
+    'shape.anchor': 'top_left',  # or bottom_left, top_right, bottom_right
+    'shape.path' : [],
+    'shape.left': 0.0,
+    'shape.top': 0.0,
+    'shape.width': 400.0,
+    'shape.height': 400.0,
+    'shape.cornersx': 4,
+    'shape.cornersy': 4,
+    'border': False,
+    'borderwidth.normal': 0,
+    'borderwidth.hovered': 0,
+    'borderwidth.clicked': 0,
+    'bordercolor.normal': '#888888',
+    'bordercolor.hovered': '#888888',
+    'bordercolor.clicked': '#888888',
+    'bordercolor.transparency': 0,
+    'bgcolor.normal': '#888888',
+    'bgcolor.hovered': '#888888',
+    'bgcolor.clicked': '#888888',
+    'bgcolor.transparency': 0,
+    'text.content': '',
+    'text.size': 12,
+    'text.bold': False,
+    'text.italic': False,
+    'text.color': '#FFFFFF',
+    'text.valign': 'center',  # or 'top' or bottom
+    'text.halign': 'center',  # or 'left' or 'right'
+    'action.targets': [],
+    'action.commands': [],
+    'action.menu_commands': [],
+    'image.path': '',
+    'image.fit': True,
+    'image.ratio': False,
+    'image.height': 32,
+    'image.width': 32,
+}
 
 
 def rgb_to_hex(r, g, b):
@@ -93,6 +241,7 @@ def build_picker_from_pkr(title, buttons, imagepath, dst):
         picker['shapes'].insert(0, image_to_background_shape(imagepath))
     frame_picker_buttons(picker)
     fit_picker_to_content(picker)
+    ensure_retro_compatibility(picker)
     with open(dst, "w") as f:
         json.dump(picker, f, indent=2)
 
